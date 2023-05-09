d9723a4c5a77baa7e2adb1c3d88d5410f959fe60
Taher Shaikh <115086728+tahers755@users.noreply.github.com>
2023-05-09 09:10:15 +0530

    Update CalculatorTest.java

--- src/test/java/com/github/stokito/unitTestExample/calculator/CalculatorTest.java
+++ src/test/java/com/github/stokito/unitTestExample/calculator/CalculatorTest.java
@@ -1,37 +1,36 @@
 package com.github.stokito.unitTestExample.calculator;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
 
-	@Test
-	public void testSum() {
-		// Given
-		Calculator calculator = new Calculator();
-		// When
-		int result = calculator.sum(2, 2);
-		// Then
-		if (result != 4) {   // if 2 + 2 != 4
-			Assert.fail();
-		}
-	}
+    @Test
+    public void testSum() {
+        // Given
+        Calculator calculator = new Calculator();
+        // When
+        int result = calculator.sum(2, 2);
+        // Then
+        Assert.assertEquals(4, result);
+    }
 
-	@Test
-	public void testMinus() {
-		Calculator calculator = new Calculator();
-		Assert.assertEquals(0, calculator.minus(2, 2));
-	}
+    @Test
+    public void testMinus() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(0, calculator.minus(2, 2));
+    }
 
-	@Test
-	public void testDivide() {
-		Calculator calculator = new Calculator();
-		Assert.assertEquals(2, calculator.divide(6, 3));
-	}
+    @Test
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(2, calculator.divide(6, 3));
+    }
 
-	@Test(expected = ArithmeticException.class)
-	public void testDivideWillThrowExceptionWhenDivideOnZero() {
-		Calculator calculator = new Calculator();
-		calculator.divide(6, 0);
-	}
+    @Test(expected = ArithmeticException.class)
+    public void testDivideWillThrowExceptionWhenDivideOnZero() {
+        Calculator calculator = new Calculator();
+        calculator.divide(6, 0);
+    }
 }
+
